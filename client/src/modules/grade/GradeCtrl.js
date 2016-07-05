a234bf1fdc7f9f156c9c3c6732af55fb938c5041
Karmasakshi Goyal <karmasakshi121@gmail.com>
2016-07-05 18:34:42 +0530

    Add updating grade.

--- client/src/modules/grade/GradeCtrl.js
+++ client/src/modules/grade/GradeCtrl.js
@@ -8,7 +8,10 @@ angular
 
         /* --- MODELS --- */
 
+        // $scope.grade;
         // $scope.grades;
+        // $scope.isEditorActive;
+        // $scope.isNascent;
 
         /* --- FUNCTIONS --- */
 
@@ -17,17 +20,61 @@ angular
             // Set page title
             $rootScope.pageTitle = 'Grade - Attendancer';
 
+            $scope.grade = null;
+
             $scope.grades = [];
+
+            $scope.isEditorActive = false;
 
             $scope.getGrades();
 
         };
 
+        $scope.activateEditor = function (i) {
+
+            $scope.isEditorActive = true;
+
+            $scope.grade = $scope.grades[i];
+
+        };
+
+        $scope.deactivateEditor = function () {
+
+            $scope.isEditorActive = false;
+
+            $scope.grade = null;
+
+        }
+
         $scope.getGrades = function () {
 
-            Grade.list({institutionId: 1}).$promise.then(function (response) {
+            Grade.list({ institutionId: 1 }).$promise.then(function (response) {
 
                 $scope.grades = response;
+
+            }, function () {
+
+                $rootScope.notify('Failed.');
+
+            });
+
+        };
+
+        $scope.updateGrade = function () {
+
+            $scope.isNascent = true;
+
+            $scope.grade.$save().then(function () {
+
+                $scope.deactivateEditor();
+
+                $scope.isNascent = false;
+
+            }, function () {
+
+                $rootScope.notify('Failed.');
+
+                $scope.isNascent = false;
 
             });
 
